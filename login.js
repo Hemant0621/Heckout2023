f3500e7012c527974cd7abf044b68538f0d66ac0
Hemant0621 <132387936+Hemant0621@users.noreply.github.com>
2023-11-04 22:40:22 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const loginForm = document.getElementById("login-form");
-  
+    const errorMessage = document.getElementById("error-message");
     
     
     loginForm.addEventListener("submit", function (event) {
