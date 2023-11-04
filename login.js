b0ce7f41f0a2163a515ff7626aac26c47b724ade
Hemant0621 <132387936+Hemant0621@users.noreply.github.com>
2023-11-04 10:11:16 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -9,8 +9,9 @@ document.addEventListener("DOMContentLoaded", function () {
         const password = document.getElementById("password").value;
 
         // You can add your login validation logic here
-        if (email === "your@email.com" && password === "yourpassword") {
+        if (email === "admin@gmail.com" && password === "admin") {
             errorMessage.textContent = "";
+            window.location.href="/dashboard.html"
             // Redirect to the user's dashboard or perform login logic
         } else {
             errorMessage.textContent = "Invalid email or password";
